73449181171c2b5a667b2f20d99922a4c38d2cda
Jorge Flores <flores.jorgeluis@gmail.com>
2023-03-27 19:21:41 -0600

    first commit

---
+++ src/pages/contactos.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "@/styles/Contactos.module.css";
+
+function contactos() {
+  return (
+    <div>
+      <h1>Bienvenidos a Contactos</h1>
+    </div>
+  );
+}
+
+export default contactos;
---
+++ src/pages/usuarios.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function usuarios() {
+  return <div>Bienvenido a usuarios</div>;
+}
+
+export default usuarios;
